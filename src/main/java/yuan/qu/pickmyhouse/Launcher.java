4d0003fa469d333ecbb3aba7c9eade16b01b05bb
quyuan <Qy180301@>
2019-06-28 21:08:01 +0800

    init

---
+++ src/main/java/yuan/qu/pickmyhouse/Launcher.java
@@ -0,0 +1,15 @@
+package yuan.qu.pickmyhouse;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(new Object[]{Launcher.class}, args);
+    }
+
+}
